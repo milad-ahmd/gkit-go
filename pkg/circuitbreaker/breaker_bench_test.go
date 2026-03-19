705482a9cee5d062dda773e63ba23dee144fe882
Milad Ahmadi <miladahmadi803@gmail.com>
2026-03-19 18:59:12 +0330

    bench(circuitbreaker): Execute and StateCheck benchmarks

---
+++ pkg/circuitbreaker/breaker_bench_test.go
@@ -0,0 +1,29 @@
+package circuitbreaker_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/miladhzz/gkit/pkg/circuitbreaker"
+)
+
+func BenchmarkCircuitBreaker_Execute_Closed(b *testing.B) {
+	cb := circuitbreaker.New(circuitbreaker.WithFailureThreshold(100))
+	ctx := context.Background()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_, _ = circuitbreaker.Execute(ctx, cb, func(ctx context.Context) (int, error) {
+				return 1, nil
+			})
+		}
+	})
+}
+
+func BenchmarkCircuitBreaker_StateCheck(b *testing.B) {
+	cb := circuitbreaker.New()
+	b.ResetTimer()
+	for b.Loop() {
+		_ = cb.State()
+	}
+}
