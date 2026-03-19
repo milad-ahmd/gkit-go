192bfa0b05a1bb4a86a826778af21fa5f3f06042
Milad Ahmadi <miladahmadi803@gmail.com>
2026-03-19 18:59:12 +0330

    bench(ratelimit): Allow, AllowN, and keyed limiter benchmarks

---
+++ pkg/ratelimit/limiter_bench_test.go
@@ -0,0 +1,38 @@
+package ratelimit_test
+
+import (
+	"testing"
+
+	"github.com/miladhzz/gkit/pkg/ratelimit"
+)
+
+func BenchmarkRateLimiter_Allow(b *testing.B) {
+	lim := ratelimit.New(1_000_000, 1_000_000) // high limits so we don't block
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lim.Allow()
+		}
+	})
+}
+
+func BenchmarkRateLimiter_AllowN(b *testing.B) {
+	lim := ratelimit.New(1_000_000, 1_000_000)
+	b.ResetTimer()
+	for b.Loop() {
+		lim.AllowN(1)
+	}
+}
+
+func BenchmarkKeyedRateLimiter_Allow(b *testing.B) {
+	klim := ratelimit.NewKeyed[string](1_000_000, 1_000_000)
+	keys := []string{"user-a", "user-b", "user-c", "user-d"}
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			klim.Allow(keys[i%len(keys)])
+			i++
+		}
+	})
+}
